e499eea53c882752d3db0e2f7bab9cef3cb0263a
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2020-09-28 15:03:27 +0200

    hello world

---
+++ handshake/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace handshake.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class HelloWorldController : ControllerBase
+  {
+    [HttpGet]
+    public string Get()
+    {
+      return "Hello World! Hallo Blue! Hallo Max!";
+    }
+  }
+}
