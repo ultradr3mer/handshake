57e4bb7647b328d1ccf7784b9cb09168d4aaf112
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2020-09-28 16:34:03 +0200

    database select

--- handshake/Controllers/HelloWorldController.cs
+++ handshake/Controllers/HelloWorldController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 
 namespace handshake.Controllers
@@ -7,10 +10,32 @@ namespace handshake.Controllers
   [Route("[controller]")]
   public class HelloWorldController : ControllerBase
   {
+    string connectionString = "Server=tcp:server2.database.windows.net,1433;Initial Catalog=handshake;Persist Security Info=False;User ID=Name;Password=5LQazj2DTtz6e32;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
     [HttpGet]
     public string Get()
     {
-      return "Hello World! Hallo Blue! Hallo Max!";
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        connection.Open();
+        string sql = "SELECT NAME, NAME2, AGE FROM PEOPLE";
+        using (SqlCommand command = new SqlCommand(sql, connection))
+        {
+          using (SqlDataReader reader = command.ExecuteReader())
+          {
+            StringBuilder sb = new StringBuilder();
+
+            while (reader.Read())
+            {
+              sb.Append(reader.GetString(0));
+              sb.Append(' ');
+              sb.AppendLine(reader.GetString(1));
+            }
+
+            return sb.ToString();
+          }
+        }
+      }
     }
   }
 }
